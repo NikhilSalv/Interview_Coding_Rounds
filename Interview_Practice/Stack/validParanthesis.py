12a9fbda9e3cf9196f52ba6b82dd3dfb656b8c2b
NikhilSalv <NikhilSalvi@123>
2025-05-24 16:08:39 +0100

    valid paranthesis

---
+++ Interview_Practice/Stack/validParanthesis.py
@@ -0,0 +1,32 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+def validParenthesis(s):
+    mapping = {")":"(","}":"{","]":"["}
+    stack = []
+    if len(s) < 2:
+        return False
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping:
+            if not stack or stack.pop() != mapping[char]:
+                return False
+        else:
+            return False
+    if stack:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    s= "()[(})]"
+    s1 = "(]"
+    print(validParenthesis(s))
